b748bb8769895ece9a0afd935dea737967ced16a
Thunnini <yunjh1994@gmail.com>
2020-10-25 11:07:42 +0900

    Add the decorator to the background

--- src/layouts/header-layout/index.tsx
+++ src/layouts/header-layout/index.tsx
@@ -4,6 +4,10 @@ import { Container, Navbar, NavbarBrand } from "reactstrap";
 import { observer } from "mobx-react";
 import { useStore } from "../../stores";
 
+import style from "./style.module.scss";
+
+import classnames from "classnames";
+
 export const HeaderLayout: FunctionComponent = observer(props => {
   const { children } = props;
 
@@ -11,6 +15,13 @@ export const HeaderLayout: FunctionComponent = observer(props => {
 
   return (
     <div>
+      <div className={classnames(style.squares, style.square1)} />
+      <div className={classnames(style.squares, style.square2)} />
+      <div className={classnames(style.squares, style.square3)} />
+      <div className={classnames(style.squares, style.square4)} />
+      <div className={classnames(style.squares, style.square5)} />
+      <div className={classnames(style.squares, style.square6)} />
+      <div className={classnames(style.squares, style.square7)} />
       <Navbar
         expand="lg"
         color="transparent"
